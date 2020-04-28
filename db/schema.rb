ec6720b0ad25c401fe02919b0bd652cb076eb3d6
Maxim van Hoorn <mvanhoorn1@babson.edu>
2020-04-28 12:51:57 +0200

    finished comment functionality

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,15 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_04_27_120957) do
+ActiveRecord::Schema.define(version: 2020_04_28_094244) do
+
+  create_table "comments", force: :cascade do |t|
+    t.string "name"
+    t.text "comment"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.integer "post_id"
+  end
 
   create_table "posts", force: :cascade do |t|
     t.string "title"
